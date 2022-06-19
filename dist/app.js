174ec55e67ed5aaa92737b74db8d7d77af40a72b
Arvind Kushwaha <arvindkshwh939@gmail.com>
2022-06-19 17:19:35 +0530

    set source and dist directory

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,14 @@
+"use strict";
 // const test = "this is test log";
 // // document.write(test); // use this line if 'defer' is removed in index.html
 // console.info(test);
-
 class Friuts {
-    constructor(public id: number, public name: string, private type: string) {
-        console.log(`I am eating ${id} ${name} in Constructor which is ${type} food`);
+    constructor(id, name, type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        console.log(`I am eating ${id} ${name} in Constructor which is ${type} food !!`);
     }
 }
-
 const firstFruite = new Friuts(1, 'Banana', 'healthy');
-
 const secondFruite = new Friuts(4, 'Apple', 'more healthy');
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// const test = "this is test log";
+// // document.write(test); // use this line if 'defer' is removed in index.html
+// console.info(test);
+
+class Friuts {
+    constructor(public id: number, public name: string, private type: string) {
+        console.log(`I am eating ${id} ${name} in Constructor which is ${type} food !!`);
+    }
+}
+
+const firstFruite = new Friuts(1, 'Banana', 'healthy');
+
+const secondFruite = new Friuts(4, 'Apple', 'more healthy');
