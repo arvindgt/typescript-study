d3edbdf3bc5b47a6e66617c49317b783ead0997c
Arvind Kushwaha <arvindkshwh939@gmail.com>
2022-06-19 16:26:27 +0530

    lite-server setup to run

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var test = "this is test log";
-document.write(test);
+document.write(test); // use this line if 'defer' is removed in index.html
 console.info(test);
--- app.ts
+++ app.ts
@@ -1,2 +1,4 @@
 const test = "this is test log";
+// document.write(test); // use this line if 'defer' is removed in index.html
+
 console.info(test);
